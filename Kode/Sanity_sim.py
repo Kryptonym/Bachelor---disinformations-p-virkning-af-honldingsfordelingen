f830ef8be65bda531e8d6c87b496fcc6ba54b9f4
Kryptonym <rassmuzz.hn@gmail.com>
2026-04-28 12:37:03 +0200

    retellser af små fejl

--- Kode/Sanity_sim.py
+++ Kode/Sanity_sim.py
@@ -12,31 +12,33 @@ import networkx as nx
 import networkx.utils as nx_utils
 print('Done loading functions')
 
-steps = 50
+steps = 100
 print('Gen, af grafer med normalfordelte holdninger')
-Graf_random_normal =  random_relations_graph(50,50)
-Graf_ba_normal = ba_opinion_graph()
-Graf_ws_normal = ws_opinion_graph()
-Graf_hk_normal = hk_opinion_graph()
-Graf_sbm_normal = sbm_opinion_graph()
+Graf_random_normal =  random_relations_graph(1000,150)
+Graf_ba_normal = ba_opinion_graph(1000,2)
+Graf_ws_normal = ws_opinion_graph(1000)
+Graf_hk_normal = hk_opinion_graph(1000)
+Graf_sbm_normal = sbm_opinion_graph([1000,1000])
+
+#degreedist(Graf_ba_normal,"spring")
 
 print('Gen, af grafer med uniformfordelte holdninger')
-Graf_random_uni =  random_relations_uniform_graph(50,50)
-Graf_ba_uni = ba_opinion_uniform_graph()
-Graf_hk_uni = hk_opinion_uniform_graph()
-Graf_ws_uni = ws_opinion_uniform_graph()
-Graf_sbm_uni = sbm_opinion_uniform_graph()
+Graf_random_uni =  random_relations_uniform_graph(1000,150)
+Graf_ba_uni = ba_opinion_uniform_graph(1000,2)
+Graf_hk_uni = hk_opinion_uniform_graph(1000)
+Graf_ws_uni = ws_opinion_uniform_graph(1000)
+Graf_sbm_uni = sbm_opinion_uniform_graph([1000,1000])
 
 print('Gen, af grafer med polart holdninger')
-Graf_random_polar =  random_relations_polar_graph(50,50)
-Graf_ba_polar = ba_opinion_polar_graph()
-Graf_ws_polar = ws_opinion_polar_graph()
-Graf_hk_polar = hk_opinion_polar_graph()
-Graf_sbm_polar = sbm_opinion_polar_graph()
+Graf_random_polar =  random_relations_polar_graph(1000,150)
+Graf_ba_polar = ba_opinion_polar_graph(1000,2)
+Graf_ws_polar = ws_opinion_polar_graph(1000)
+Graf_hk_polar = hk_opinion_polar_graph(1000)
+Graf_sbm_polar = sbm_opinion_polar_graph([1000,1000])
 
 
 print("tilfældig graf")
-start_cond, end_state, all_avg_opinions,all_avg_distances = simple_simulation(Graf_random_uni,steps)
+start_cond, end_state, all_avg_opinions,all_avg_distances = simple_simulation(Graf_random_normal,steps)
 save_simulation_results(start_cond, end_state,all_avg_opinions,all_avg_distances,'Random_normal','png','resultater/sanity/normal')
 start_cond, end_state, all_avg_opinions,all_avg_distances = simple_simulation(Graf_random_uni,steps)
 save_simulation_results(start_cond, end_state,all_avg_opinions,all_avg_distances,'Random_uniform','png','resultater/sanity/uniform')
