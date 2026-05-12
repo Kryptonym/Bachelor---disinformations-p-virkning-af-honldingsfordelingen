28db6efcc43b055a3fa9dcc86c5bf4409486e0b3
Kryptonym <rassmuzz.hn@gmail.com>
2026-05-12 11:32:50 +0200

    .

--- Kode/Matrix_test.py
+++ Kode/Matrix_test.py
@@ -8,16 +8,17 @@ np.random.seed(11)
 from Sanity_graf import *
 from Matrix_graph import *
 from Matrix_simulering import *
-from Simulering import save_simulation_results
+from Simulering import *
 
 print("start")
-A =  sbm_opinion_graph([5000,5000])
+A =  sbm_opinion_graph([500,500])
 print("matrix gen")
 B ,_,_= create_matrix_rep(A)
 
 print("Sim")
-
 start_cond, end_state,all_avg_opinions,all_avg_distances = simple_simulation_matrix(A,100)
+
+
 
 save_simulation_results_matrix(start_cond, end_state,all_avg_opinions,all_avg_distances,'Ingen_medier_ingen_disinfo_ikke_dynamiske_kanter','png','resultater/simulering_matrix')
 print("done")
